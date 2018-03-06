acdbf9e33c9b98295082936bae983a9cabdd8721
prafull <prafull.dhadkar@gmail.com>
2018-03-06 22:23:32 +0530

    second push

---
+++ streaming.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'))
+  });
+
+app.get('/video', function (req,res) {
+    const link = 'assets/sample.mkv';
+    //const link = path.join(__dirname, 'assets/sample.mp4');
+    const stat = fs.statSync(link);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    console.log("range : ",range);
+
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+        const chunksize = (end - start) + 1;
+        const file = fs.createReadStream(link, { start, end });
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(206, head);
+        file.pipe(res);
+        console.log('Piping',parts,start,end,chunksize);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(link).pipe(res)
+        console.log('in else part');
+    }
+
+
+});
+//process.env.PORT
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000!')
+  })
