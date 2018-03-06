37c6e66e87241b2f2dcf83d18232cd64dac0ec48
prafull <prafull.dhadkar@gmail.com>
2018-03-06 23:25:11 +0530

    port change push

--- streaming.js
+++ streaming.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 var app = express();
 
-var PORT = process.env.port||3000;
+var PORT = process.env.PORT||3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
